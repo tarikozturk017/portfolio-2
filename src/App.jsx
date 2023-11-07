64a625cc987af6e287ebc635b32f38d59d498bf6
tarikozturk017 <tarik.ozturkk1@gmail.com>
2023-11-06 21:24:35 -0500

    services and homepage update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+
 const App = () => {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section>Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,34 @@
+import "./portfolio.scss";
+
+const items = [
+  {
+    id: 1,
+    title: "Project Name",
+    img: "https://www.tarikozturk.ca/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcuriee.f4d32e0f.gif&w=1200&q=75",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolore, eveniet cumque iure veritatis aliquam excepturi aperiam, laborum cupiditate quibusdam ut quam odio explicabo exercitationem? Vel dolorem non nostrum tempore.",
+  },
+  {
+    id: 2,
+    title: "Project Name",
+    img: "https://www.tarikozturk.ca/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcuriee.f4d32e0f.gif&w=1200&q=75",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolore, eveniet cumque iure veritatis aliquam excepturi aperiam, laborum cupiditate quibusdam ut quam odio explicabo exercitationem? Vel dolorem non nostrum tempore.",
+  },
+  {
+    id: 3,
+    title: "Project Name",
+    img: "https://www.tarikozturk.ca/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcuriee.f4d32e0f.gif&w=1200&q=75",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolore, eveniet cumque iure veritatis aliquam excepturi aperiam, laborum cupiditate quibusdam ut quam odio explicabo exercitationem? Vel dolorem non nostrum tempore.",
+  },
+  {
+    id: 4,
+    title: "Project Name",
+    img: "https://www.tarikozturk.ca/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcuriee.f4d32e0f.gif&w=1200&q=75",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolore, eveniet cumque iure veritatis aliquam excepturi aperiam, laborum cupiditate quibusdam ut quam odio explicabo exercitationem? Vel dolorem non nostrum tempore.",
+  },
+];
+
+const Portfolio = () => {
+  return <div className="portfolio">Portfolio</div>;
+};
+
+export default Portfolio;
